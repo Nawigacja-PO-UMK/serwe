7aeb84b0ddac8a8ad9e764b7c0a8787d0420c36f
matbloch2 <115151767+matbloch2@users.noreply.github.com>
2023-01-24 21:53:21 +0100

    mongodb

--- index.php
+++ index.php
@@ -1,24 +1,17 @@
 <?php
+require 'vendor/autoload.php';
 
 if (isset($_POST['dane'])) {
     $odebranedane= $_POST['dane'];
-    if(filesize("baza_pozycji.jos")!=0)
-    {
-        $dane = fread(fopen("baza_pozycji.jos", "r"), filesize("baza_pozycji.jos"));
-        $skany=json_decode($dane);
-        $skan=json_decode($odebranedane);
-        $result = array_merge($skany,$skan);
-        $dane=json_encode($result);
-    }
-   else
-        $dane=$odebranedane;
+    $client = new MongoDB\Client("mongodb://localhost:27017");
+    $dane=json_decode($odebranedane);
+    foreach ($dane as $key => $skany)
+                $client->skany->skany_pozycji->insertone($skany);
 
-    $fp = fopen("baza_pozycji.jos", "w");
-    fputs($fp,$dane);
-    fclose($fp);
     echo  "dane wysłane";
   }
   else {
     echo "bład wysłania";
   }
  ?>
+
